2ad4437caf0fb55b026e5ec53c6ad88991b5118d
Chin Rui <chewchinrui@gmail.com>
2018-03-21 22:06:05 +0800

    Added zip file commands

---
+++ testrun3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run accelerometer data collection script
+python accel_collect_data.py > logs/accel_log &
+
+# Run GPS data collection script
+python gps_collect_data.py > logs/gps_log &
+
+# Run set system time script 
+python set_system_time.py > logs/system_time_log &
+
+# Run data writing script
+python collect_data.py > logs/collect_log &
+
+# Run zip file script
+# python zip_file.py > logs/zip_log &
--- zip_file.py
+++ zip_file.py
@@ -13,12 +13,15 @@ def zip_file():
             if (time_now > time_dir):
                 # zipped_path = config.zipped_dir + dir + '.gz'
                 zipped_path = config.zipped_dir + dir + '.zip'
+                print(zipped_path)
                 data_path = config.data_dir +  dir
+                print(data_path)
                 # zip -r compressed_filename.zip foldername
-                os.system('cd ' + config.zipped_dir)
-                os.system('sudo zip -r ' + dir + '.zip' + ' ' + data_path)
+                os.system('cd ' + data_path)
+                # os.system('sudo zip -r ' + zipped_path + ' *')
+                os.system('sudo zip ' + zipped_path + ' *')
                 # os.system('sudo tar -czf ' + zipped_path + ' ' +  data_path)
-                os.system('sudo rm -r ' + data_path)
+                # os.system('sudo rm -r ' + data_path)
 
 if __name__ == "__main__":
     zip_file()
